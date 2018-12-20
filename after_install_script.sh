f0a3d442c116880e3b451eb547bc01bcd26c16a5
zakriya7 <45287390+zakriya7@users.noreply.github.com>
2018-12-20 02:46:48 -0800

    Update after_install_script.sh

--- after_install_script.sh
+++ after_install_script.sh
@@ -1,5 +1,5 @@
 #!bin/bash
- cd /var/www/port81
+ cd /usr/share/nginx/html/nodejs
  curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
  yum -y install nodejs > /dev/null 2> /dev/null < /dev/null &
  npm start > /dev/null 2> /dev/null < /dev/null &
