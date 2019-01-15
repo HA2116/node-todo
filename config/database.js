ae2a03fae91facfca14fb058429f1b252789a1a6
zakriya7 <45287390+zakriya7@users.noreply.github.com>
2019-01-15 15:03:14 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://34.243.225.71:27017/meanstacktutorials'
+    localUrl: 'mongodb://localhost/meanstacktutorials'
 };
